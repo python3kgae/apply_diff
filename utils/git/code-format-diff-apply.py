6314c0d257cadcde181b7a46d4fe6917a8967eb5
Xiang Li <python3kgae@outlook.com>
2023-11-06 12:20:48 -0500

    Read diff from comment.

--- utils/git/code-format-diff-apply.py
+++ utils/git/code-format-diff-apply.py
@@ -10,6 +10,7 @@
 
 import argparse
 import os
+import re
 import subprocess
 import sys
 import tempfile
@@ -18,27 +19,36 @@ from functools import cached_property
 import github
 from github import IssueComment, PullRequest
 
+diff_pat = re.compile(r"``````````diff(?P<DIFF>.+)``````````", re.DOTALL)
 
 def apply_patches(args: argparse.Namespace) -> None:
     repo = github.Github(args.token).get_repo(args.repo)
     pr = repo.get_issue(args.issue_number).as_pull_request()
+    comment = pr.get_issue_comment(args.comment_id)
+    if comment is None:
+        raise(f"Comment {args.comment_id} does not exist")
 
-    patch_path = os.path.join("/home/runner/work/format",f"{args.issue_number}")
-    if not os.path.exists(patch_path):
-        raise(f"Patch path {patch_path} does not exist")
+    # get the diff from the comment
+    m = re.search(diff_pat, comment.body)
+    if m is None:
+        raise(f"Could not find diff in comment {args.comment_id}")
+    diff = m.group("DIFF")
 
-    for patch_file in os.listdir(patch_path):
-        if patch_file.endswith(".patch"):
-            # run git apply patch_file
-            apply_cmd = [
-                "git",
-                "apply",
-                os.path.join(patch_path, patch_file)
-            ]
-            print(f"Running: {' '.join(apply_cmd)}")
-            proc = subprocess.run(apply_cmd, capture_output=True)
-            if proc.returncode != 0:
-                raise(f"Failed to apply patch {patch_file}")
+    # create a temporary file
+    with tempfile.NamedTemporaryFile() as tmp:
+        tmp.write(diff.encode("utf-8"))
+        tmp.flush()
+
+        # run git apply tmp.name
+        apply_cmd = [
+            "git",
+            "apply",
+            tmp.name
+        ]
+        print(f"Running: {' '.join(apply_cmd)}")
+        proc = subprocess.run(apply_cmd, capture_output=True)
+        if proc.returncode != 0:
+            raise(f"Failed to apply diff from comment {args.comment_id}")
 
     # run git add .
     add_cmd = [
@@ -64,6 +74,7 @@ if __name__ == "__main__":
         help="The GitHub repository that we are working with in the form of <owner>/<repo> (e.g. llvm/llvm-project)",
     )
     parser.add_argument("--issue-number", type=int, required=True)
+    parser.add_argument("--comment-id", type=int, required=True)
 
     args = parser.parse_args()
 
